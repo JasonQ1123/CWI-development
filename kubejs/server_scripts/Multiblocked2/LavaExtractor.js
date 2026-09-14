45372d3cb647b7d94ce6bfc2b384d07d50e06793
QIUWH <1537326617@qq.com>
2026-09-14 23:34:45 +0800

    CWI Beta 0.4.1

--- kubejs/server_scripts/Multiblocked2/LavaExtractor.js
+++ kubejs/server_scripts/Multiblocked2/LavaExtractor.js
@@ -1,4 +1,4 @@
-// On Machine Tick
+// Lava Extractor
 
 MBDMachineEvents.onTick('cwi:lava_extractor_head', event => {
     if (globalTickCounter % 40) return
@@ -21,15 +21,22 @@ MBDMachineEvents.onTick('cwi:lava_extractor_head', event => {
 
     if (speed < 128) return
 
-    const extractAmount = Math.round( 500 * speed / 256)
+    let pumpCount = 0
+
+    machine.getMultiblockState().getCache().forEach(pos => {
+        if (level.getBlockState(pos).getBlock().equals(Block.getBlock('cwi:lava_extractor_pump'))) pumpCount++
+    })
+
+    const extractAmount = Math.round( pumpCount * 500 * speed / 256)
     const isMagmaBedrock = blockBelow == 'kubejs:eruptive_bedrock'
     const fluidTank = machine.getTraitByName("fluid_tank")
 
     if (isMagmaBedrock) {
         fluidTank.storages[0].setFluid(Fluid.of('minecraft:lava', extractAmount))
         level.playSound(null, pos.x + 0.5, pos.y + 0.5, pos.z + 0.5, 'minecraft:item.bucket.empty_lava', 'blocks', soundMultiplier, 0.5)
-        for (let i = 0; i < 4; i++) {
-            level.spawnParticles('clanginghowl:breakdown_smoke', true, blockAbovePos.x + random(0, 1), blockAbovePos.y - 0.2, blockAbovePos.z + random(0, 1), 0, 0, 0, 0, 0)
+        for (let i = 0; i < 3 + pumpCount * 2; i++) {
+            level.spawnParticles('clanginghowl:breakdown_smoke', true, blockAbovePos.x + random(0, 1), blockAbovePos.y - 0.2 + random(0, pumpCount - 1), blockAbovePos.z + random(0, 1), 0, 0, 0, 0, 0)
+            level.spawnParticles('clanginghowl:flamethrower_burst', true, blockAbovePos.x + random(0, 1), blockAbovePos.y + random(0, pumpCount - 1) / 2, blockAbovePos.z + random(0, 1), 0, 0, 0, 0, 0)
             level.spawnParticles('minecraft:smoke', true, blockAbovePos.x + random(0, 1), blockAbovePos.y, blockAbovePos.z + random(0, 1), 0, 0, 0, 0, 0)
         }
     }
