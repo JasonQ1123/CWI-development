ed220e2f4218127dec18c9d3dd0f7dcb370499a2
QIUWH <1537326617@qq.com>
2026-09-19 16:08:50 +0800

    CWI Beta 0.4.1

--- kubejs/server_scripts/Multiblocked2/LavaExtractor.js
+++ kubejs/server_scripts/Multiblocked2/LavaExtractor.js
@@ -4,32 +4,24 @@ MBDMachineEvents.onTick('cwi:lava_extractor_head', event => {
     if (globalTickCounter % 40) return
 
     const machine  = event.getEvent().getMachine()
+
+    if (!machine.customData.getBoolean('structureFormed')) return
+
     const level = machine.getLevel()
     const pos = machine.getPos()
-    const blockBelowPos = pos.offset(0, -1, 0)
     const blockAbovePos = pos.offset(0, 1, 0)
-    const blockBelow = level.getBlock(blockBelowPos)
 
     const partmachine = $IMachine.ofMachine(level, blockAbovePos).orElse(false)
-    const holder = partmachine.machineHolder
-    if (!holder) return
-    const speed = holder.speed
-    if (!speed) return
+    const speed = partmachine.machineHolder.speed
     const soundMultiplier = speed / MAX_RPM
 
     level.playSound(null, pos.x + 0.5, pos.y + 0.5, pos.z + 0.5, 'clanginghowl:drilling', 'blocks', soundMultiplier / 2, 0.25)
 
     if (speed < 128) return
 
-    let pumpCount = 0
-
-    machine.getMultiblockState().getCache().forEach(pos => {
-        if (level.getBlockState(pos).getBlock().equals(Block.getBlock('cwi:lava_extractor_pump'))) pumpCount++
-    })
-
+    const pumpCount = machine.customData.getInt('pumpCount')
     const extractAmount = Math.round( pumpCount * 500 * speed / MAX_RPM)
-    const isMagmaBedrock = blockBelow == 'kubejs:eruptive_bedrock'
-    const fluidTank = machine.getTraitByName("fluid_tank")
+    const fluidTank = machine.getTraitByName("fluid_tank").storages[0]
 
     const particleCount = 3 + 2 * pumpCount
     const particleOffset = Math.sqrt(pumpCount - 1) / 4
@@ -39,9 +31,44 @@ MBDMachineEvents.onTick('cwi:lava_extractor_head', event => {
 
     level.spawnParticles('clanginghowl:breakdown_smoke', true, particleX, particleY, particleZ, 0.22, particleOffset, 0.22, particleCount, 0)
 
-    if (isMagmaBedrock) {
-        fluidTank.storages[0].setFluid(Fluid.of('minecraft:lava', extractAmount))
+    if (machine.customData.getBoolean('isMagmaBedrock')) {
+
         level.playSound(null, pos.x + 0.5, pos.y + 0.5, pos.z + 0.5, 'minecraft:item.bucket.empty_lava', 'blocks', soundMultiplier, 0.5)
         level.spawnParticles('clanginghowl:flamethrower_burst', true, particleX, particleY, particleZ, 0.22, particleOffset, 0.22, particleCount, 0)
+
+        const fluidAmount = fluidTank.getFluid().getAmount()
+        const fluidType = fluidTank.getFluid().getFluid().getFluidType()
+
+        if (fluidAmount === 0 || fluidType !== 'minecraft:lava') fluidTank.setFluid(Fluid.of('minecraft:lava', extractAmount))
+        else if (fluidAmount + extractAmount >= 4000) fluidTank.setFluid(Fluid.of('minecraft:lava', 4000))
+        else fluidTank.getFluid().grow(extractAmount)
+    
     }
+})
+
+MBDMachineEvents.onStructureFormed('cwi:lava_extractor_head', event => {
+
+    const machine  = event.getEvent().getMachine()
+    const level = machine.getLevel()
+    const pos = machine.getPos()
+    const blockBelowPos = pos.offset(0, -1, 0)
+    const blockBelow = level.getBlock(blockBelowPos)
+
+    let pumpCount = 0
+
+    machine.getMultiblockState().getCache().forEach(pos => {
+        if (level.getBlockState(pos).getBlock().equals(Block.getBlock('cwi:lava_extractor_pump'))) pumpCount++
+    })
+
+    machine.customData.putInt('pumpCount', pumpCount)
+    machine.customData.putBoolean('structureFormed', true)
+    machine.customData.putBoolean('isMagmaBedrock', blockBelow == 'kubejs:eruptive_bedrock')
+
+})
+
+MBDMachineEvents.onStructureInvalid('cwi:lava_extractor_head', event => {
+
+    const machine  = event.getEvent().getMachine()
+    machine.customData.putBoolean('structureFormed', false)
+
 })
